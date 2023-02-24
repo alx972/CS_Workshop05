efeda5ccec2bd77cced118c66c69c6efd3d5c046
Alex Slyussarev <alex.slyussarev@yandex.ru>
2023-02-24 23:23:02 +0300

    task #1: generate array method added

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 
-int[] GenerateArray(int length, int minValue, int maxValue)
+int[] GenerateArray(int length, int minValue, int maxValue) // заполнение случайными числами массива с заданными характеристиками
 {
     Random rnd = new Random();
     int[] result = new int[length];
@@ -12,8 +12,30 @@ int[] GenerateArray(int length, int minValue, int maxValue)
     return result;
 }
 
+void PrintArray(int[] array) // вывод на экран массива
+{
+    System.Console.Write($"[{array[0]}");
+    for (int i=1; i < array.Length; i++)
+    {
+        System.Console.Write($", {array[i]}");
+    }
+    System.Console.WriteLine("]");
+}
+
+int CountPositiveNumbersInArray(int[] array) // подсчет положительных чисел в массиве
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
 int size = 10;
 int minValue = 100;
 int maxValue = 1000;
 int[] myArray = new int[size];
 myArray = GenerateArray(size, minValue, maxValue);
+PrintArray(myArray);
+System.Console.WriteLine($"Количество положительных чисел в массиве = {CountPositiveNumbersInArray(myArray)}");
