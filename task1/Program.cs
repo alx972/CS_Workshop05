4144ec8308fa71aa7fb64fae178c02649016a83e
Alex Slyussarev <alex.slyussarev@yandex.ru>
2023-02-24 23:03:29 +0300

    task #1: generate array method added

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,19 @@
 ﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] GenerateArray(int length, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] result = new int[length];
+    for (int i=0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int size = 10;
+int minValue = 100;
+int maxValue = 1000;
+int[] myArray = new int[size];
+myArray = GenerateArray(size, minValue, maxValue);
